81fc809b21802e2abc8bc93ce215dfdbe3db03c9
midCenturyDude7 <mgriffes@gmail.com>
2022-01-24 20:18:38 -0800

    Con't to set up React with class-based components

--- services/client/flask-react-auth-app/src/index.js
+++ services/client/flask-react-auth-app/src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+/* import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+*/
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column-is-one-third">
+              <br />
+              <h1 className="title is-1">Users</h1>
+              <hr/><br/>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+};
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
